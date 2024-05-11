48d8651c2b98bcb5e38e9c3b55d302d06d146a4a
chrou <chrou@grandstream.cn>
2024-05-11 18:00:59 +0800

    add chrome.manifest.omnibox sample

--- LOG.md
+++ LOG.md
@@ -1,3 +1,7 @@
+
+* 2024-5-11
+  * add `chrome.manifest.omnibox` sample
+
 * 2024-5-6 
   * add `chrome.desktopCapture` sample
   * add `chrome.tabCapture.getMediaStreamId` sample
---
+++ chrome.manifest.omnibox/background.js
@@ -0,0 +1,106 @@
+const BASE_URL = "https://searchfox.org/mozilla-central";
+const SEARCH_URL = `${BASE_URL}/search`;
+const SOURCE_URL = `${BASE_URL}/source`;
+
+let nameSpace = chrome || browser;
+nameSpace.tabs = undefined;
+
+// Provide help text to the user.
+nameSpace.omnibox.setDefaultSuggestion({
+  description: `Search the firefox codebase
+    (e.g. "hello world" | "path:omnibox.js onInputChanged")`
+});
+
+// Update the suggestions whenever the input is changed.
+nameSpace.omnibox.onInputChanged.addListener((text, addSuggestions) => {
+  console.log('text:', text);
+  console.log('addSuggestions:', addSuggestions);
+  let headers = new Headers({"Accept": "application/json"});
+  let init = {method: 'GET', headers};
+  let url = buildSearchURL(text);
+  console.log('get guild search url:', url)
+  let request = new Request(url, init);
+
+  fetch(request)
+      .then(createSuggestionsFromResponse)
+      .then(addSuggestions);
+});
+
+// Open the page based on how the user clicks on a suggestion.
+nameSpace.omnibox.onInputEntered.addListener((text, disposition) => {
+  let url = text;
+  if (!text.startsWith(SOURCE_URL)) {
+    // Update the url if the user clicks on the default suggestion.
+    url = `${SEARCH_URL}?q=${text}`;
+  }
+  switch (disposition) {
+    case "currentTab":
+      nameSpace.tabs.update({url});
+      break;
+    case "newForegroundTab":
+      nameSpace.tabs.create({url});
+      break;
+    case "newBackgroundTab":
+      nameSpace.tabs.create({url, active: false});
+      break;
+  }
+});
+
+function buildSearchURL(text) {
+  let path = '';
+  let queryParts = [];
+  let query = '';
+  let parts = text.split(' ');
+
+  parts.forEach(part => {
+    if (part.startsWith("path:")) {
+      path = part.slice(5);
+    } else {
+      queryParts.push(part);
+    }
+  });
+
+  query = queryParts.join(' ');
+  return `${SEARCH_URL}?q=${query}&path=${path}`;
+}
+
+function createSuggestionsFromResponse(response) {
+  return new Promise(resolve => {
+    let suggestions = [];
+    let suggestionsOnEmptyResults = [{
+      content: SOURCE_URL,
+      description: "no results found"
+    }];
+    response.json().then(json => {
+      if (!json.normal) {
+        return resolve(suggestionsOnEmptyResults);
+      }
+
+      let occurrences = json.normal["Textual Occurrences"];
+      let files = json.normal["Files"];
+
+      if (!occurrences && !files) {
+        return resolve(suggestionsOnEmptyResults);
+      }
+
+      if (occurrences) {
+        occurrences.forEach(({path, lines}) => {
+          suggestions.push({
+            content: `${SOURCE_URL}/${path}#${lines[0].lno}`,
+            description: lines[0].line,
+          });
+        });
+        return resolve(suggestions);
+      }
+
+      // There won't be any textual occurrences if the "path:" prefix is used.
+      files.forEach(({path}) => {
+        suggestions.push({
+          content: `${SOURCE_URL}/${path}`,
+          description: path,
+        });
+      });
+      return resolve(suggestions);
+    });
+  });
+}
