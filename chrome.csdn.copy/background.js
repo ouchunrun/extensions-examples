9cfd7a98fc73a687735db5d00196bcb96ebdf217
chrou <chrou@grandstream.cn>
2024-07-30 09:44:19 +0800

    update code

--- chrome.csdn.copy/background.js
+++ chrome.csdn.copy/background.js
@@ -2,7 +2,7 @@
 chrome.action.onClicked.addListener((tab) => {
     console.log("Executing code when the plugin icon is clicked.");
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'executeCode' }, function(response) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'cancelUserSelectNone' }, function(response) {
             console.log(response);
         });
     });
--- chrome.csdn.copy/content.js
+++ chrome.csdn.copy/content.js
@@ -1,22 +1,4 @@
-function setAllSelect(el = document.body) {
-    for (let index = 0; index < el.children.length; index++) {
-        const e = el.children.item(index);
-        e.style.userSelect = 'text';
-        setAllSelect(e);
-    }
-}
 
-document.documentElement.addEventListener('mouseup', e => {
-    const pasteText = window.getSelection().toString();
-    if (null === pasteText || undefined === pasteText || '' === pasteText.trim()) {
-        return;
-    }
-    navigator.clipboard.writeText(pasteText).then(() => {
-        console.log('复制成功！', pasteText)
-    }).catch((e) => {
-        console.log('复制失败！', e)
-    });
-});
 
 let nameSpace
 try {
@@ -31,14 +13,44 @@ try {
     }
 }
 
+/**
+ * 监听背景页发送的消息
+ */
 if (nameSpace && nameSpace.runtime && nameSpace.runtime.onMessage) {
     console.log('set runtime onMessage listener')
     nameSpace.runtime.onMessage.addListener(function (request, sender, sendResponse){
         console.log('runtime onMessage', request)
-        if (request && request.action === 'executeCode') {
-            setAllSelect()
+        if (request && request.action === 'cancelUserSelectNone') {
+            console.log('set user-select to text!!!!!!!')
+            setUserSelectToText()
         }
         sendResponse('request success')
     })
 }
 
+/**
+ * 1、取消所有节点的user-select:none 设置
+ * @param el
+ */
+function setUserSelectToText(el = document.body) {
+    for (let index = 0; index < el.children.length; index++) {
+        const e = el.children.item(index);
+        e.style.userSelect = 'text';
+        setUserSelectToText(e);
+    }
+}
+
+/**
+ * 2.鼠标松开时，复制选中的文本
+ */
+document.documentElement.addEventListener('mouseup', e => {
+    const pasteText = window.getSelection().toString();
+    if (null === pasteText || undefined === pasteText || '' === pasteText.trim()) {
+        return;
+    }
+    navigator.clipboard.writeText(pasteText).then(() => {
+        console.log('复制成功！', pasteText)
+    }).catch((e) => {
+        console.log('复制失败！', e)
+    });
+});
